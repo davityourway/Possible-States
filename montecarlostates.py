aa613feed170a3ddb4ddbb7487270415cf0a5a24
davityourway <Joshua.Davitz@gmail.com>
2021-03-08 15:49:55 -0500

    added a function to check legality and also to check the proportion of terminal and illegal games

--- montecarlostates.py
+++ montecarlostates.py
@@ -24,7 +24,7 @@ class MonteCarlo:
         self.root = self.make_root(m, n)
         self.term_states = {x: 0 for x in range(1, n*m + 1)}
         self.non_term_states = {x: 0 for x in range(1, n*m + 1)}
-        # self.illegal_states = {x: 0 for x in range(1, n*m + 1)}
+        self.illegal_states = {x: 0 for x in range(1, n*m + 1)}
         # self.avg_game_depth = 0
         self.samples_generated = 0
         self.states_per_turn = self.find_states_per_turn(n * m)
@@ -32,9 +32,10 @@ class MonteCarlo:
 
     def play_game(self):
         """
-        plays a full game of and then continues sampling to ensure an unbiased sample of possible board states at
-        each timestep. We currently treat terminal states and illegal states the same way
-        when incrementing the terminal state dictionary :return:
+        plays a full game until we reach a terminal state, after which we check each state for its legality. If a state
+        has an illegal configuration we pop the game stack and increment the illegal states dictionary by one for each
+        subsequent turn
+         :return:
         """
         game = rand.sample(self.all_positions, len(self.all_positions))
         curr = self.make_root(self.m, self.n)
@@ -50,12 +51,20 @@ class MonteCarlo:
                 curr.player = next_player
                 if curr.terminal:
                     self.term_states[depth] += 1
-                    winner = "First" if curr.player == "Second" else "Second"
                     win_move = move
                 else:
                     self.non_term_states[depth] += 1
             else:
-                illegal
+                curr.legal = self.check_legal(curr.positions, move, win_move, curr.player)
+                curr.player = next_player
+                if not curr.legal:
+                    self.illegal_states[depth] += 1
+                    while game:
+                        depth += 1
+                        game.pop()
+                        self.illegal_states[depth] += 1
+                else:
+                    self.term_states[depth] += 1
 
     def check_terminal(self, positions: List[List[str]], move: Tuple[int, int], player: str):
         """
@@ -76,6 +85,26 @@ class MonteCarlo:
             totals.append(total)
         return True if max(totals) >= self.k else False
 
+    def check_legal(self, positions: List[List[str]], move: Tuple[int, int], win_move: Tuple[int, int], player: str):
+        target = "1" if player == "First" else "2"
+        directions = [(1, 0, -1, 0), (0, 1, 0, -1), (1, 1, -1, -1), (-1, 1, 1, -1)]
+        for direction in directions:
+            total = 1
+            fcount, fset = self.target_in_direction(move, (direction[0], direction[1]), positions, target)
+            fset.add(move)
+            f_has_win = True if win_move in fset else False
+            if fcount+1 >= self.k and not f_has_win or fcount > self.k*2 - 2:
+                return False
+            bcount, bset = self.target_in_direction(move, (direction[2], direction[3]), positions, target)
+            bset.add(move)
+            b_has_win = True if win_move in bset else False
+            if bcount+1 >= self.k and not b_has_win or bcount > self.k*2 - 2:
+                return False
+            total += fcount + bcount
+            if total >= self.k and not (b_has_win or f_has_win) or total >= self.k*2:
+                return False
+        return True
+
     def target_in_direction(self, start: Tuple[int, int], direction: Tuple[int, int], positions: List[List[str]], target: str):
         """
         returns the number of a target letter in a row
@@ -88,12 +117,12 @@ class MonteCarlo:
         increment = 1
         total = 0
         coordinate = (direction[0]*increment + start[0], direction[1]*increment + start[1])
-        checked = {coordinate}
+        checked = set()
         while 0 <= coordinate[0] < self.m and 0 <= coordinate[1] < self.n and positions[coordinate[0]][coordinate[1]] == target:
+            checked.add(coordinate)
             total += 1
             increment += 1
             coordinate = (direction[0] * increment + start[0], direction[1] * increment + start[1])
-            checked.add(coordinate)
         return total, checked
 
     def make_root(self, m: int, n: int):
@@ -128,7 +157,7 @@ class MonteCarlo:
         calculates an estimate of the total number of non-terminal states, updates the object parameter
         :return:
         """
-        proportions = estimate_proportions(self)
+        proportions = estimate_nonterm_proportions(self)
         self.non_term_estimate = 0
         for turn in range(1, self.max_moves):
             self.non_term_estimate += proportions[turn] * self.states_per_turn[turn]
@@ -142,10 +171,11 @@ class MonteCarlo:
         """
 
         for i in range(n):
-            # if not i % 5000:
-                # print(self.samples_generated)
-                # print(self.non_term_states)
-                # print(self.term_states)
+            if not i % 5000:
+                print(self.samples_generated)
+                print(self.non_term_states)
+                print(self.term_states)
+                print(self.illegal_states)
             if i > 1 and not (i % 25000):
                 self.update_non_term_estimate()
                 print(self.non_term_estimate)
@@ -162,7 +192,7 @@ class BoardState:
         self.legal = legal
 
 
-def estimate_proportions(mc_record: MonteCarlo):
+def estimate_nonterm_proportions(mc_record: MonteCarlo):
     """
 
     :param mc_record:
@@ -173,26 +203,49 @@ def estimate_proportions(mc_record: MonteCarlo):
     for turn in range(1, mc_record.max_moves+1):
         non_term = mc_record.non_term_states[turn]
         term = mc_record.term_states[turn]
-        prop_nt = non_term/(term+non_term)
+        illegal = mc_record.illegal_states[turn]
+        prop_nt = non_term/(term+non_term+illegal)
+        proportions.append(prop_nt)
+    return proportions
+
+
+def estimate_term_proportions(mc_record: MonteCarlo):
+    """
+
+    :param mc_record:
+    :return: array of the proportion of states at each time step
+    Gives the proportion of terminal states at each step in an array, which can be used as a probability estimate
+    """
+    proportions = [1]
+    for turn in range(1, mc_record.max_moves+1):
+        non_term = mc_record.non_term_states[turn]
+        term = mc_record.term_states[turn]
+        illegal = mc_record.illegal_states[turn]
+        prop_nt = term/(term+non_term+illegal)
+        proportions.append(prop_nt)
+    return proportions
+
+
+def estimate_illegal_proportions(mc_record: MonteCarlo):
+    """
+
+    :param mc_record:
+    :return: array of the proportion of states at each time step
+    Gives the proportion of illegal states at each step in an array, which can be used as a probability estimate
+    """
+    proportions = [1]
+    for turn in range(1, mc_record.max_moves+1):
+        non_term = mc_record.non_term_states[turn]
+        term = mc_record.term_states[turn]
+        illegal = mc_record.illegal_states[turn]
+        prop_nt = illegal/(term+non_term+illegal)
         proportions.append(prop_nt)
     return proportions
 
 
 
-
-
 a = MonteCarlo(4, 4, 9)
-a.simulate_n_games(1000000)
-
-# current best estimate at 1e7 samples per timestep is 1.3312354544798618e+16
-# print(estimate_proportions(a))
-# positions = 4
-# n = 1
-# m = 0
-# turn = 1
-# print(factorial(positions) / (factorial(n) * factorial(m) * factorial(positions - turn)))
-# positions = 4
-# n = 1
-# m = 1
-# turn = 2
-# print(factorial(positions) / (factorial(n) * factorial(m) * factorial(positions - turn)))
+a.simulate_n_games(100000)
+print(estimate_nonterm_proportions(a))
+print(estimate_term_proportions(a))
+print(estimate_illegal_proportions(a))
