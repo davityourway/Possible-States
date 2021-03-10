5e2d3d17385e6b74f12df7c1708bf5f280810370
davityourway <Joshua.Davitz@gmail.com>
2021-03-09 19:21:22 -0500

    legality check works

--- montecarlostates.py
+++ montecarlostates.py
@@ -1,7 +1,16 @@
 import itertools
 import random as rand
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Set
 from math import factorial
+
+
+def find_legal_junctions(win_list: List[Tuple[int, int]], k: int):
+    win_size = len(win_list)
+    distance_from_end = win_size - k
+    if win_size == k:
+        return set(win_list)
+    else:
+        return set(win_list[distance_from_end:-distance_from_end])
 
 
 class MonteCarlo:
@@ -47,15 +56,15 @@ class MonteCarlo:
             next_player = "First" if curr.player == "Second" else "Second"
             curr.positions[move[0]][move[1]] = "1" if curr.player == "First" else "2"
             if not curr.terminal:
-                curr.terminal = self.check_terminal(curr.positions, move, curr.player) if not curr.terminal else True
+                curr.terminal, win_list = self.check_terminal(curr.positions, move, curr.player)
                 curr.player = next_player
                 if curr.terminal:
                     self.term_states[depth] += 1
-                    win_move = move
+                    win_set = find_legal_junctions(win_list, self.k)
                 else:
                     self.non_term_states[depth] += 1
             else:
-                curr.legal = self.check_legal(curr.positions, move, win_move, curr.player)
+                curr.legal = self.check_legal(curr.positions, move, win_list, win_set, curr.player)
                 curr.player = next_player
                 if not curr.legal:
                     self.illegal_states[depth] += 1
@@ -76,36 +85,46 @@ class MonteCarlo:
         """
         target = "1" if player == "First" else "2"
         directions = [(1, 0, -1, 0), (0, 1, 0, -1), (1, 1, -1, -1), (-1, 1, 1, -1)]
-        totals = []
         for direction in directions:
             total = 1
-            fcount, fset = self.target_in_direction(move, (direction[0], direction[1]), positions, target)
-            bcount, bset = self.target_in_direction(move, (direction[2], direction[3]), positions, target)
-            total += fcount + bcount
-            totals.append(total)
-        return True if max(totals) >= self.k else False
+            fcount, fchecked = self.target_in_direction(move, (direction[0], direction[1]), positions, target)
+            bcount, bchecked = self.target_in_direction(move, (direction[2], direction[3]), positions, target)
+            if total + fcount + bcount >= self.k:
+                return True, bchecked + [move] + fchecked
+        return False, []
 
-    def check_legal(self, positions: List[List[str]], move: Tuple[int, int], win_move: Tuple[int, int], player: str):
+    def check_legal(self, positions: List[List[str]], move: Tuple[int, int], win_list: List, win_set: Set, player: str):
+        """
+        function checks the legality by searching for a winning combination that is either larger than the maximum length
+        (k*2 - 1) or does not contain the original winning move (which would create two independent win states on the
+        board).
+        :param positions:
+        :param move:
+        :param win_list:
+        :param player:
+        :return:
+        """
         target = "1" if player == "First" else "2"
-        directions = [(1, 0, -1, 0), (0, 1, 0, -1), (1, 1, -1, -1), (-1, 1, 1, -1)]
-        for direction in directions:
+        direction_pairs = [(1, 0, -1, 0), (0, 1, 0, -1), (1, 1, -1, -1), (-1, 1, 1, -1)]
+        for direction in direction_pairs:
             total = 1
-            fcount, fset = self.target_in_direction(move, (direction[0], direction[1]), positions, target)
-            fset.add(move)
-            f_has_win = True if win_move in fset else False
-            if fcount+1 >= self.k and not f_has_win or fcount > self.k*2 - 2:
-                return False
-            bcount, bset = self.target_in_direction(move, (direction[2], direction[3]), positions, target)
-            bset.add(move)
-            b_has_win = True if win_move in bset else False
-            if bcount+1 >= self.k and not b_has_win or bcount > self.k*2 - 2:
-                return False
+            fcount, fchecked = self.target_in_direction(move, (direction[0], direction[1]), positions, target)
+            bcount, bchecked = self.target_in_direction(move, (direction[2], direction[3]), positions, target)
             total += fcount + bcount
-            if total >= self.k and not (b_has_win or f_has_win) or total >= self.k*2:
-                return False
+            if total >= self.k:
+                win_sample = win_list[0]
+                if total >= self.k*2 or target != positions[win_sample[0]][win_sample[1]]:
+                    return False
+                new_win_list = bchecked + [move] + fchecked
+                new_win_junctions = find_legal_junctions(new_win_list, self.k)
+                win_intersection = new_win_junctions & win_set
+                if not win_intersection:
+                    return False
+                win_set = win_intersection
         return True
 
-    def target_in_direction(self, start: Tuple[int, int], direction: Tuple[int, int], positions: List[List[str]], target: str):
+    def target_in_direction(self, start: Tuple[int, int], direction: Tuple[int, int], positions: List[List[str]],
+                            target: str, win_move: Optional[Tuple[int, int]] = None):
         """
         returns the number of a target letter in a row
         :param start: non inclusive beginning of the search
@@ -117,9 +136,9 @@ class MonteCarlo:
         increment = 1
         total = 0
         coordinate = (direction[0]*increment + start[0], direction[1]*increment + start[1])
-        checked = set()
+        checked = []
         while 0 <= coordinate[0] < self.m and 0 <= coordinate[1] < self.n and positions[coordinate[0]][coordinate[1]] == target:
-            checked.add(coordinate)
+            checked.append(coordinate)
             total += 1
             increment += 1
             coordinate = (direction[0] * increment + start[0], direction[1] * increment + start[1])
@@ -243,9 +262,10 @@ def estimate_illegal_proportions(mc_record: MonteCarlo):
     return proportions
 
 
+if __name__ == '__main__':
 
-a = MonteCarlo(4, 4, 9)
-a.simulate_n_games(100000)
-print(estimate_nonterm_proportions(a))
-print(estimate_term_proportions(a))
-print(estimate_illegal_proportions(a))
+    a = MonteCarlo(5, 10, 10)
+    a.simulate_n_games(100000)
+    print(estimate_nonterm_proportions(a))
+    print(estimate_term_proportions(a))
+    print(estimate_illegal_proportions(a))
